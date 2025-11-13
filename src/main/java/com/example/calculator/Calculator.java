05739f9f3ef4732a7408701223b558c4c1b02493
glanoop2 <gl.anoop2@gmail.com>
2025-11-13 21:43:46 +0530

    mul fixed

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
         return a - b;
     }
     public int multiply(int a, int b) {
-        return a * a;
+        return a * b;
     }
     public int divide(int a, int b) {
         if (b == 0) {
